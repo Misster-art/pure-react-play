af51132136656f41174caa8efc1f851b00e90813
Misster-art <ja981uqhq@gmail.com>
2025-01-30 13:49:27 +0430

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,50 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#007AFF", // iOS blue
+          foreground: "#FFFFFF",
+        },
+        secondary: {
+          DEFAULT: "#8E9196", // iOS gray
+          foreground: "#FFFFFF",
+        },
+        ios: {
+          gray: "#8E9196",
+          lightGray: "#F1F1F1",
+          darkGray: "#333333",
+          coolGray: "#aaadb0",
+        }
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
